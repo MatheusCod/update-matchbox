9d5c4dd43079742d04ed9d669c08aeb851e1ba56
MatheusCod <41351032+MatheusCod@users.noreply.github.com>
2020-09-10 14:40:38 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,18 +4,17 @@ LOCALPATH=$TRAVIS_BUILD_DIR
 REPO1="/teste/matheus"
 
 cd $LOCALPATH
-#wget https://github.com/poseidon/matchbox/archive/v0.8.3.zip
+wget https://github.com/poseidon/matchbox/archive/v0.8.3.zip
 #wget https://github.com/poseidon/matchbox/archive/v0.8.0.zip
-wget https://github.com/poseidon/matchbox/archive/v0.7.1.zip
-#unzip v0.8.3.zip
+#wget https://github.com/poseidon/matchbox/archive/v0.7.1.zip
+unzip v0.8.3.zip
 #unzip v0.8.0.zip
-unzip v0.7.1.zip
+#unzip v0.7.1.zip
 
-#cd matchbox-0.8.3
-#make
-#cd bin
-#mv matchbox matchbox-0.8.3
-#echo "Terminou 1"
+cd matchbox-0.8.3
+make
+cd bin
+mv matchbox matchbox-0.8.3
 
 #cd $LOCALPATH
 
@@ -23,19 +22,18 @@ unzip v0.7.1.zip
 #make
 #cd bin
 #mv matchbox matchbox-0.8.0
-#echo "Terminou 2"
 
 #cd $LOCALPATH
 
-cd matchbox-0.7.1
-echo "Antes do make"
-ls
-make
-cd bin
-mv matchbox matchbox-0.7.1
-echo "Arquivos"
-ls
+#cd matchbox-0.7.1
+#echo "Antes do make"
+#ls
+#make
+#cd bin
+#mv matchbox matchbox-0.7.1
+#echo "Arquivos"
+#ls
 
-#lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O $REPO1 $LOCALPATH/matchbox-0.8.3/bin/matchbox-0.8.3"
+lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O $REPO1 $LOCALPATH/matchbox-0.8.3/bin/matchbox-0.8.3"
 #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O $REPO1 $LOCALPATH/matchbox-0.8.0/bin/matchbox-0.8.0"
-lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O $REPO1 $LOCALPATH/matchbox-0.7.1/bin/matchbox-0.7.1"
+#lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O $REPO1 $LOCALPATH/matchbox-0.7.1/bin/matchbox-0.7.1"
