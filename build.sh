2432d953e38d569eb0ecd8c91e289a6573c23d3c
MatheusCod <41351032+MatheusCod@users.noreply.github.com>
2020-09-10 13:59:00 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ cd matchbox-0.8.3
 make
 cd bin
 mv matchbox matchbox-0.8.3
-lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O $REPO1 $LOCALPATH/matchbox-0.8.3"
+lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O $REPO1 $LOCALPATH/bin/matchbox-0.8.3"
 
 #wget https://github.com/poseidon/matchbox/archive/v0.8.3.zip
 #wget https://github.com/poseidon/matchbox/archive/v0.8.0.zip
