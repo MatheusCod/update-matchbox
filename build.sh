1efccf93797044b26d0933ed6ae8bb9eaea9dc15
MatheusCod <41351032+MatheusCod@users.noreply.github.com>
2020-09-10 17:08:57 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ cd $LOCALPATH
 echo "Antes"
 ls
 echo "Depois"
+mkdir matchbox
+cd matchbox
 #wget https://github.com/poseidon/matchbox/archive/v0.8.3.zip
 #wget https://github.com/poseidon/matchbox/archive/v0.8.0.zip
 wget https://github.com/poseidon/matchbox/archive/v0.7.1.zip
