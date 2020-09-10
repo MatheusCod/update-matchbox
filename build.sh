8b96f06a825c08f364ef2171d370c69be2e920d2
MatheusCod <41351032+MatheusCod@users.noreply.github.com>
2020-09-10 18:12:57 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,16 +6,16 @@ REPO1="/teste/matheus"
 
 cd $LOCALPATH
 # 0.8.3
-#git clone https://github.com/poseidon/matchbox.git
-#cd matchbox
-#git checkout v0.8.3
-
-# 0.8.0
-mkdir coreos
-cd coreos
 git clone https://github.com/poseidon/matchbox.git
 cd matchbox
-git checkout v0.8.0
+git checkout v0.8.3
+
+# 0.8.0
+#mkdir coreos
+#cd coreos
+#git clone https://github.com/poseidon/matchbox.git
+#cd matchbox
+#git checkout v0.8.0
 
 # 0.7.1
 #mkdir coreos
@@ -24,17 +24,17 @@ git checkout v0.8.0
 #cd matchbox
 #git checkout v0.8.0
 
-#make
-#cd bin
-#mv matchbox matchbox-0.8.3
-#./matchbox-0.8.3 -version
+make
+cd bin
+mv matchbox matchbox-0.8.3
+./matchbox-0.8.3 -version
 
 #cd $LOCALPATH
 
-make
-cd bin
-mv matchbox matchbox-0.8.0
-./matchbox-0.8.0 -version
+#make
+#cd bin
+#mv matchbox matchbox-0.8.0
+#./matchbox-0.8.0 -version
 
 #cd $LOCALPATH
 
@@ -43,6 +43,6 @@ mv matchbox matchbox-0.8.0
 #mv matchbox matchbox-0.7.1
 #./matchbox-0.7.1 -version
 
-#lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O $REPO1 $LOCALPATH/coreos/matchbox/bin/matchbox-0.8.3"
-lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O $REPO1 $LOCALPATH/coreos/matchbox/bin/matchbox-0.8.0"
+lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O $REPO1 $LOCALPATH/matchbox/bin/matchbox-0.8.3"
+#lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O $REPO1 $LOCALPATH/coreos/matchbox/bin/matchbox-0.8.0"
 #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O $REPO1 $LOCALPATH/coreos/matchbox/bin/matchbox-0.7.1"
