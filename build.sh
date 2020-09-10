bdf151b70e3b0d067c48da93928a1fcf1442b2d3
MatheusCod <41351032+MatheusCod@users.noreply.github.com>
2020-09-10 13:35:12 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
-
+#!/bin/bash
 
 LOCALPATH=$GOPATH/src
 
 wget https://github.com/poseidon/matchbox/archive/v0.8.3.zip
 unzip v0.8.3.zip
-ls
+cd matchbox-0.8.3
+make
+./bin/matchbox -version
 
 #wget https://github.com/poseidon/matchbox/archive/v0.8.3.zip
 #wget https://github.com/poseidon/matchbox/archive/v0.8.0.zip
@@ -13,3 +15,7 @@ ls
 #unzip v0.8.3.zip
 #unzip v0.8.0.zip
 #unzip v0.7.1.zip
+
+cd matchbox-0.8.3
+make
+./bin/matchbox -version
