411b6d0f62eae9dc4411abe14c7d8b7065998365
MatheusCod <41351032+MatheusCod@users.noreply.github.com>
2020-09-10 13:19:41 -0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+LOCALPATH=$GOPATH/src
+
+wget https://github.com/poseidon/matchbox/archive/v0.8.3.zip
+unzip v0.8.3.zip
+ls
+
+#wget https://github.com/poseidon/matchbox/archive/v0.8.3.zip
+#wget https://github.com/poseidon/matchbox/archive/v0.8.0.zip
+#wget https://github.com/poseidon/matchbox/archive/v0.7.1.zip
+
+#unzip v0.8.3.zip
+#unzip v0.8.0.zip
+#unzip v0.7.1.zip
